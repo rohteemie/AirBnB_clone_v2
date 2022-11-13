3a5e955990d1ba3ed33b7f56b835196fb4f128de
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2022-11-13 14:33:43 +0100

    Hot fix, correction for 3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-from flask import Flask, escape
+"""
+starts a Flask web application
+"""
+
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    '''
-    Hello route
-    '''
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    '''
-    HBNB route
-    '''
-    return 'HBNB:'
+    """returns HBNB"""
+    return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_text(text):
-    '''
-    /c/<text> route
-    '''
-    return 'C {}'.format(text.replace('_', ' '))
-
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-    app.run()
-    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port='5000')
